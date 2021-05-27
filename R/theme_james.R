389cb0f6db1cc68b95c4443c8112fd51258f0bb4
James P. Hare <james@jamesphare.org>
2021-05-27 15:19:52 -0400

    add comments

--- R/theme_james.R
+++ R/theme_james.R
@@ -4,6 +4,7 @@
 #'
 #' @export
 theme_james <- ggplot2::theme_minimal() +
-  ggplot2::theme(legend.position = 'top',
-                 text = ggplot2::element_text(family = 'Helvetica'),
-                 panel.grid = ggplot2::element_blank())
+  ggplot2::theme(legend.position = 'top', #put legend on top
+                 text = ggplot2::element_text(family = 'Helvetica'), #set font
+                 panel.grid = ggplot2::element_blank() #remove grid
+                 ) 
