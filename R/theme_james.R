7a294fcc782b5c0bc086b52abc8769a2c7f286b0
James P. Hare <james@jamesphare.org>
2021-05-27 17:22:14 -0400

    further customized theme

--- R/theme_james.R
+++ R/theme_james.R
@@ -3,19 +3,21 @@
 #' My custom GGPlot2 Theme
 #'
 #' @export
-theme_james <- function() {
+theme_james <- function(){
   #start building theme based on theme_minimal
   ggplot2::theme_minimal() +
     
     ggplot2::theme(
-      
-      #put legend on top
-      legend.position = 'top',
+      #put legend on bottom
+      legend.position = 'bottom',
       
       #set font
       text = ggplot2::element_text(family = 'Helvetica'),
       
       #remove grid
-      panel.grid = ggplot2::element_blank()
+      panel.grid = ggplot2::element_blank(),
+      
+      #remove legend title
+      legend.title = ggplot2::element_blank(),
     )
 }
