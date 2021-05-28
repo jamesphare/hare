103be5528754138a10da5e8b70903bbfb80de7a9
James P. Hare <james@jamesphare.org>
2021-05-28 15:30:14 -0400

    Added example to documentation

--- R/theme_james.R
+++ R/theme_james.R
@@ -1,8 +1,14 @@
-#' Custom GGPlot2 Theme
-#'
-#' My custom GGPlot2 Theme
-#'
+#' Theme James
+#' 
+#' My custom GGPlot2 theme
+#' 
 #' @export
+#'
+#' @examples
+#' library(ggplot2)
+#' ggplot(mpg, aes(displ, hwy, colour = class)) + 
+#' geom_point() + 
+#' theme_james()
 theme_james <- function(){
   #start building theme based on theme_minimal
   ggplot2::theme_minimal() +
